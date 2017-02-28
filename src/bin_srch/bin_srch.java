ef79da779a3bf0980a5b0d3aab641195888e1ecf
levro <levro@ukr.net>
2017-02-28 10:44:49 +0200

    yesterday

--- src/bin_srch/bin_srch.java
+++ src/bin_srch/bin_srch.java
@@ -17,5 +17,7 @@ public class bin_srch {
 		System.out.println(output);
 		System.out.println("Thank you");
 		System.out.println("----------------");
+		System.out.println("yesterday all my troubles seem so far away");
+		System.out.println("now i look");
 	}
 }
